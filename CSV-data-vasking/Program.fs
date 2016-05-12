7ea627b79c8f49ea519d55813c80cd5d914d1d57
Vegard Harper <vegardh@mesan.no>
2016-05-12 17:53:23 +0200

    visualizing statoil stock prize

--- CSV-data-vasking/Program.fs
+++ CSV-data-vasking/Program.fs
@@ -1,13 +1,25 @@
-﻿
+﻿open FSharp.Data
+open FSharp.Charting
+open System.Windows.Forms
+open System
 
+Application.EnableVisualStyles()
+Application.SetCompatibleTextRenderingDefault false
 
+[<Literal>]
+let url = "http://ichart.finance.yahoo.com/table.csv?s=STO"
 
+type Statoil = CsvProvider<url, CacheRows=false>
 
+let stocks = Statoil.GetSample()
 
-
+let chart = 
+    Chart.Line([ for row in stocks.Rows -> row.Date, row.Open ])
 
 
 [<EntryPoint>]
+[<STAThread>]
 let main argv = 
-    printfn "%A" argv
+
+    Application.Run(chart.ShowChart())
     0
