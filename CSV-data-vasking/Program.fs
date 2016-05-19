84f0ae719ba3aea970231a7ab018916e9b38625e
Vegard Harper <vegardh@mesan.no>
2016-05-19 20:39:25 +0200

    using maps and sequences instead

--- CSV-data-vasking/Program.fs
+++ CSV-data-vasking/Program.fs
@@ -1,42 +1,24 @@
-﻿namespace lol
-
-open FSharp.Data
-open FSharp.Charting
-open System.Windows.Forms
+﻿open FSharp.Data
 open System
 
-module lol = 
-    Application.EnableVisualStyles()
-    Application.SetCompatibleTextRenderingDefault false
-    
-    [<Literal>]
-    let url = "http://ichart.finance.yahoo.com/table.csv?s=STO"
-    
-    [<Literal>]
-    let url1 = "http://ichart.finance.yahoo.com/table.csv?s=DNB.OL"
-    
-    [<Literal>]
-    let url2 = "http://ichart.finance.yahoo.com/table.csv?s=TEL.OL"
-    
-    type Statoil = CsvProvider<url, CacheRows=false>
-    
-    let statoil = Statoil.GetSample()
-    
-    type Dnb = CsvProvider<url1, CacheRows=false>
-    
-    let dnb = Dnb.GetSample()
-    
-    type Telenor = CsvProvider<url2, CacheRows=false>
-    
-    let telenor = Telenor.GetSample()
-    
-    let chart = 
-        Chart.Combine([ Chart.Line([ for row in statoil.Rows -> row.Date, row.Open ])
-                        Chart.Line([ for row in dnb.Rows -> row.Date, row.Open ])
-                        Chart.Line([ for row in telenor.Rows -> row.Date, row.Open ]) ])
-    
-    [<EntryPoint>]
-    [<STAThread>]
-    let main argv = 
-        Application.Run(chart.ShowChart())
-        0
+let urlBase = "http://ichart.finance.yahoo.com/table.csv?s="
+let companyStockNames = [ "STO"; "DNB"; "TEL" ]
+let companyStockUrl company : string = urlBase + company
+let literals = List.map companyStockUrl companyStockNames
+
+for lit in literals do
+    printfn "%s" (lit)
+
+let readCsvUrl (url : string) = CsvFile.Load(url).Cache()
+let csvFiles = List.map readCsvUrl literals
+
+let firstResult = 
+    csvFiles
+    |> Seq.map (fun file -> file.Rows)
+    |> Seq.map (fun rows -> Seq.head rows)
+
+for a in firstResult do
+    printfn "%s" (a.GetColumn("High"))
+
+[<EntryPoint>]
+let main argv = 0
