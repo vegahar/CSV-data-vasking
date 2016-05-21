62263672c6037ea07b0616e23ca2ab923640cbc5
Vegard Harper <vegardh@mesan.no>
2016-05-21 18:48:15 +0200

    inline

--- CSV-data-vasking/Program.fs
+++ CSV-data-vasking/Program.fs
@@ -2,17 +2,12 @@
 open FSharp.Data
 open FSharp.Data.CsvExtensions
 
-type Close = float
-
-type Open = float
-
 type Stock = 
     { date : DateTime
-      openn : Open
+      openn : float
       high : float
       low : float
-      close : Close
-      volume : float }
+      close : float }
 
 type ClassifiedStock = 
     | StockIsDown
@@ -22,30 +17,23 @@ let classifyStock (stock : Stock) : ClassifiedStock =
     if stock.close <= stock.openn then StockIsDown
     else StockIsUp
 
-let csv = CsvFile.Load("http://ichart.finance.yahoo.com/table.csv?s=AAPL")
-let headerRow = csv.Headers
-let rows = csv.Rows
+let csv = CsvFile.Load("http://ichart.finance.yahoo.com/table.csv?s=AAPL").Rows
 
 let mapRowToStock (row : CsvRow) = 
     { date = row?Date.AsDateTime()
       openn = row?Open.AsFloat()
       high = row?High.AsFloat()
       low = row?Low.AsFloat()
-      close = row?Close.AsFloat()
-      volume = row?Volume.AsFloat() }
+      close = row?Close.AsFloat() }
 
-let stocks = Seq.map mapRowToStock rows
-let classifiedStocks = Seq.map classifyStock stocks
-
-let matchStock (stock : ClassifiedStock) = 
-    match stock with
-    | StockIsDown -> "Buy buy!!"
-    | StockIsUp -> "Sell sell!!"
-
-let groupedBy = 
-    classifiedStocks
-    |> Seq.map matchStock
+let classifiedStocks = 
+    csv
+    |> Seq.map mapRowToStock
+    |> Seq.map classifyStock
+    |> Seq.map (function 
+           | StockIsDown -> "Dårlig dag"
+           | StockIsUp -> "Bra dag")
     |> Seq.groupBy (fun word -> word)
     |> Seq.map (fun (key, value) -> key, Seq.length value)
 
-printfn "%A" groupedBy
+printfn "%A" classifiedStocks
