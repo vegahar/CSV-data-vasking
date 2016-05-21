a66bf553090a7e78a2409b709161fa6fdc3f8a71
Vegard Harper <vegardh@mesan.no>
2016-05-21 18:09:52 +0200

    moar apple stocks

--- CSV-data-vasking/Program.fs
+++ CSV-data-vasking/Program.fs
@@ -2,13 +2,28 @@
 open FSharp.Data
 open FSharp.Data.CsvExtensions
 
+type Close = float
+
+type Open = float
+
 type Stock = 
     { date : DateTime
-      openn : float
+      openn : Open
       high : float
       low : float
-      close : float
+      close : Close
       volume : float }
+
+let buybuy (stock : Stock) = stock.close <= stock.openn
+let sellsell (stock : Stock) = stock.close > stock.openn
+
+type ClassifiedStock = 
+    | Buy
+    | Sell
+
+let classifyStock (stock : Stock) : ClassifiedStock = 
+    if stock.close <= stock.openn then Buy
+    else Sell
 
 let csv = CsvFile.Load("http://ichart.finance.yahoo.com/table.csv?s=AAPL")
 let headerRow = csv.Headers
@@ -23,8 +38,15 @@ let mapRowToStock (row : CsvRow) =
       volume = row?Volume.AsFloat() }
 
 let stocks = Seq.map mapRowToStock rows
+let classifiedStocks = Seq.map classifyStock stocks
 
-for stock in stocks do
-    printfn "%A" (stock.GetType().Name)
+let matchStock (stock : ClassifiedStock) = 
+    match stock with
+    | Buy -> "Buy buy!!"
+    | Sell -> "Sell sell!!"
 
+for stock in classifiedStocks do
+    printfn "%A" (matchStock stock)
+//for stock in stocks do
+//  printfn "%A" (stock.GetType().Name)
 //Date,Open,High,Low,Close,Volume,Adj Close
